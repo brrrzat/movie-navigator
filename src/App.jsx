804d89d1e51473c4bcc41d6935818788e0669ed0
Бекзат <bekzat.sagynbekov@alatoo.edu.kg>
2026-03-15 11:45:59 +0600

    gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { MainLayout } from './layouts/MainLayout';
 import { Home } from './pages/Home';
 import { Search } from './pages/Search';
@@ -6,32 +7,17 @@ import { MovieDetails } from './pages/MovieDetails';
 import { Favorites } from './pages/Favorites';
 import { NotFound } from './pages/NotFound';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <MainLayout />,
     errorElement: <NotFound />, 
     children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'search/:query',
-        element: <Search />,
-      },
-      {
-        path: 'movie/:id',
-        element: <MovieDetails />,
-      },
-      {
-        path: 'favorites',
-        element: <Favorites />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
+      { index: true, element: <Home /> },
+      { path: 'search/:query', element: <Search /> },
+      { path: 'movie/:id', element: <MovieDetails /> },
+      { path: 'favorites', element: <Favorites /> },
+      { path: '*', element: <NotFound /> },
     ],
   },
 ]);
